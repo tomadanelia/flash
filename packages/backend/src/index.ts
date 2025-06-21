27c62ef58cff6472717d86d05119c41a5db0ffbd
Teona <teo@Teonas-MacBook-Pro.local>
2025-06-21 11:55:37 +0400

    attach socket.io to backend

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -3,11 +3,18 @@ import dotenv from 'dotenv';
 import path from 'path';
 import app from './app';
 import http from 'http'; 
+import { Server } from 'socket.io';
+import { webSocketManager } from './services/webSocketManager';
+
 
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 const port = process.env.PORT || 3001; 
 const server = http.createServer(app);
+
+
+const io = new Server(server, { cors: { origin: '*' } });
+webSocketManager.init(io);
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
